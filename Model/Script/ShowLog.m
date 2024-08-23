1ccb757d638217d9e448a1af172787c827a879d0
nearmer <nearmer@163.com>
2024-08-23 17:20:09 +0800

    update

--- Model/Script/ShowLog.m
+++ Model/Script/ShowLog.m
@@ -2,7 +2,7 @@ function  ShowLog(event)
 
 % TODO Integration the Wechat Robot Services and Bark  Services
 
-py.messageRobot.WechatsendAll(event)   
+py.messageRobot.barMessage(event)   
 disp(strcat('Info:',string(datetime),' Action :',event))
 
 end
